cc841e5280638c31c279c08cc3d07537301b09ed
Andrew Moss <agordonmoss@gmail.com>
2019-09-07 16:51:41 -0600

    More styles

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -11,17 +11,17 @@ class Navig extends React.Component {
         return (
 
             <Navbar collapseOnSelect expand="lg">
-                <Navbar.Brand href="#home">Dam</Navbar.Brand>
+                <Navbar.Brand className="logo">Dam</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="mr-auto">
+                    {/* <Nav className="mr-auto">
                         <Nav.Link href="#features">Features</Nav.Link>
                         <Nav.Link href="#pricing">Pricing</Nav.Link>
                     </Nav>
                     <Nav>
                         <Nav.Link href="#deets">More deets</Nav.Link>
                         <Nav.Link eventKey={2} href="#memes">Dank memes</Nav.Link>
-                    </Nav>
+                    </Nav> */}
                 </Navbar.Collapse>
             </Navbar>
 
